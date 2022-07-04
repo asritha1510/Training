014f18762cc54a96c7b60541b6fd213529934cc5
asritha1510 <adiddi@FTDCORP.NET>
2022-07-04 00:31:50 -0700

    Sorting salary

---
+++ Salary_Sort/emp.java
@@ -0,0 +1,28 @@
+package com.asri.objects;
+
+public class emp {
+    private String name;
+    private int age;
+    private int salary;
+
+    public void setName(String name){
+        this.name=name;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setAge(int age)
+    {
+        this.age=age;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setSal(int salary)
+    {
+        this.salary=salary;
+    }
+    public int getSal(){
+        return salary;
+    }
+}
---
+++ Salary_Sort/sortsal.java
@@ -0,0 +1,35 @@
+package com.asri.objects;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+
+public class sortsal {
+    public static void main(String[] args) {
+        emp e1=new emp();
+        e1.setName("Asri");
+        e1.setAge(22);
+        e1.setSal(30000);
+        emp e2=new emp();
+        e2.setName("Liki");
+        e2.setAge(24);
+        e2.setSal(50000);
+        emp e3=new emp();
+        e3.setName("Ani");
+        e3.setAge(20);
+        e3.setSal(10000);
+        emp e[]=new emp[3];
+        e[0]=e1;e[1]=e2;e[2]=e3;
+        Arrays.sort(e,new Comparator<emp>() {
+            @Override
+            public int compare(emp e1, emp e2) {
+                    return e1.getAge()-e2.getAge();
+                }
+            
+        });
+        for(int i=0;i<e.length;i++)
+        {
+            System.out.println(e[i].getName()+" "+e[i].getAge()+" "+e[i].getSal());
+        }
+    }
+}
